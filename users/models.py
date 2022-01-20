f8261344997d190e2739c42fe24378586539611b
gi-ong <pugo9165@gmail.com>
2022-01-20 21:40:41 +0900

    ""

---
+++ users/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    """Custom User Model"""
+
+    GEMDER_MALE = "male"
+    GEMDER_FEMALE = "female"
+    GEMDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GEMDER_MALE, "Male"),
+        (GEMDER_FEMALE, "Female"),
+        (GEMDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+
+    superhost = models.BooleanField(default=False)
